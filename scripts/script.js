0cade1b7fd2e5311eb4757e058ede703a8192d81
Danae Garcia <lindanaegarcia@gmail.com>
2024-03-15 12:17:46 -0600

    Agregar section elements

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,24 @@
-// popup edit profile
-
 let editButton = document.querySelector(".profile__edit-button");
 let popupElement = document.querySelector(".popup");
 let closeEditButton = document.querySelector(".popup__close-button");
+let saveEditButton = document.querySelector(".popup__save-button");
+
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__name");
+let jobInput = document.querySelector(".popup__about");
+let profileName = document.querySelector(".profile__name");
+let profileAbout = document.querySelector(".profile__about");
+
+let addButton = document.querySelector(".profile__add-button");
+let newPlacePopup = document.getElementById("new-image-popup");
+let createButton = document.getElementById("popup-create-btn");
+
+// popup edit profile
 
 function openPopup() {
   popupElement.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileAbout.textContent;
 }
 
 editButton.addEventListener("click", openPopup);
@@ -16,44 +29,24 @@ function closePopup() {
 
 closeEditButton.addEventListener("click", closePopup);
 
-let formElement = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__name");
-let jobInput = document.querySelector("popup__about");
-let profileName = document.querySelector(".profile__name");
-let profileAbout = document.querySelector(".profile__about");
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileAbout.textContent = jobInput.value;
+}
 
-function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
-  openPopup();
+function changeProfileInfo(event) {
+  event.preventDefault();
   nameInput.value = profileName.textContent;
   jobInput.value = profileAbout.textContent;
+  closePopup();
 }
+
+saveEditButton.addEventListener("click", changeProfileInfo);
 
 // add button
 
-let addButton = document.querySelector(".profile__add-button");
-let newPlacePopup = document.querySelector("#new-image-popup");
-let createButton = document.querySelector("#popup-create-btn");
-
-function openImagePopup() {
-  newPlacePopup.classList.add("popup__opened");
+function openNewPlacePopup() {
+  newPlacePopup.classList.add("popup_opened");
 }
-addButton.addEventListener("click", openImagePopup);
-
-// anadir places
-let elementSection = document.querySelector(".elements");
-
-function addPlace() {
-  elementSection.innerAdjacentHTML =
-    ("beforeend",
-    `<div class="element">
-  <img class="element__image" alt="image" />
-  <div class="element__info">
-    <h3 class="element__info-name"></h3>
-    <button class="card__delete-btn"></button>
-    <button class="card__like-btn"></button>
-  </div>
-</div>`);
-}
-
-addPlace.addEventListener("click", createButton);
+addButton.addEventListener("click", openNewPlacePopup);
